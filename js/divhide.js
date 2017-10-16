25fe552120c62776b39882382f94b8e34bcfdb28
albertiiito <32304946+albertiiito@users.noreply.github.com>
2017-10-16 21:54:27 +0200

    6

---
+++ js/divhide.js
@@ -0,0 +1,9 @@
+function mostrar(){
+        var x = document.getElementById("teams-div");
+            if (x.style.display === "block") {
+                x.style.display = "none";
+            } 
+            else {
+                x.style.display = "block";
+            }
+    }
